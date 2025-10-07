07bae2c83c642512db930fb6b6129bc5664b4749
Benjamin Picard <benpicar@k1r2p8.42mulhouse.fr>
2025-10-07 14:14:21 +0200

    update tests

--- srcs/requirements/MariaDB/tools/init.sql
+++ srcs/requirements/MariaDB/tools/init.sql
@@ -4,3 +4,5 @@ GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';
 CREATE USER IF NOT EXISTS 'wp_user'@'localhost' IDENTIFIED BY 'wp_password';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'qwer';
+FLUSH PRIVILEGES;
