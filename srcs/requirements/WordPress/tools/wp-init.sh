ee5f04638a94e6def717c67f057b8bfdb2646eae
Benjamin Picard <benpicar@k1r2p8.42mulhouse.fr>
2025-10-07 13:01:52 +0200

    update tests

--- srcs/requirements/WordPress/tools/wp-init.sh
+++ srcs/requirements/WordPress/tools/wp-init.sh
@@ -24,61 +24,16 @@ export WORDPRESS_DB_HOST="${WORDPRESS_DB_HOST:-mariadb}"
 
 echo "ta mere1"
 
-wp config create \
-    --dbname="$WORDPRESS_DB_NAME" \
-    --dbuser="$WORDPRESS_DB_USER" \
-    --dbpass="$WORDPRESS_DB_PASSWORD" \
-    --dbhost="$WORDPRESS_DB_HOST" \
-    --allow-root
-
-# Créer wp-config.php avant de vérifier la base
-
-echo "ta mere2"
-
-if ! wp core is-installed --allow-root; then
-  echo "ta mere5"
-  #echo "UID/GID courant : $(id -u)/$(id -g)"
-  #echo "Droits du dossier /var/www/html :"
-  #ls -ld /var/www/html
-  #echo "Contenu du dossier /var/www/html :"
-  #ls -la /var/www/html
-  #echo "Tentative de création de wp-config.php..."
-  echo "ta mere6"
+if [ ! -f wp-config.php ]; then
   wp config create \
     --dbname="$WORDPRESS_DB_NAME" \
     --dbuser="$WORDPRESS_DB_USER" \
     --dbpass="$WORDPRESS_DB_PASSWORD" \
     --dbhost="$WORDPRESS_DB_HOST" \
     --allow-root
-  status=$?
-  echo "ta mere7"
-  if [ $status -ne 0 ]; then
-    echo "Échec de la création de wp-config.php (code $status) :"
-    ls -l
-    echo "Contenu du dossier :"
-    ls -la
-    echo "Diagnostic WP-CLI :"
-    wp config create \
-      --dbname="$WORDPRESS_DB_NAME" \
-      --dbuser="$WORDPRESS_DB_USER" \
-      --dbpass="$WORDPRESS_DB_PASSWORD" \
-      --dbhost="$WORDPRESS_DB_HOST" \
-      --allow-root --debug
-    exit 1
-  fi
-
-  echo "ta mere4"
-  wp core install \
-    --url="$DOMAIN_NAME" \
-    --title="${WP_TITLE:-MonSite}" \
-    --admin_user="$WP_ADMIN_USER" \
-    --admin_password="$WP_ADMIN_PASSWORD" \
-    --admin_email="$WP_ADMIN_EMAIL" \
-    --skip-email \
-    --allow-root
-
-  wp user create "$WP_USER2" "$WP_USER2_EMAIL" --role=author --user_pass="$WP_USER2_PASSWORD" --allow-root
 fi
+
+# Créer wp-config.php avant de vérifier la base
 
 echo "ta mere3"
 
