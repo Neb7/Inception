f43658a2a934a3f9a29e43820fa56b3f5cab1fd3
Benjamin Picard <benpicar@k1r3p8.42mulhouse.fr>
2025-10-09 11:34:18 +0200

    update tests

--- srcs/requirements/MariaDB/tools/mariadb.sh
+++ srcs/requirements/MariaDB/tools/mariadb.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
+#sleep 3600
+
 # Exécute le script d'init SQL à chaque démarrage (ignorer les erreurs si la base existe déjà)
 if [ -f /docker-entrypoint-initdb.d/init.sql ]; then
     mariadb < /docker-entrypoint-initdb.d/init.sql || true
 fi
 
-exec mysqld --bind-address=0.0.0.0
+exec "$@"
