a0a2b77a09f276c759efdc2a57fb677d21e80e81
Benjamin Picard <benpicar@k1r3p8.42mulhouse.fr>
2025-10-15 17:43:10 +0200

    update tests

--- srcs/requirements/MariaDB/tools/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'wp_password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';
-CREATE USER IF NOT EXISTS 'wp_user'@'localhost' IDENTIFIED BY 'wp_password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';
-ALTER USER 'wp_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'wp_password';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'qwer' USING 'mysql_native_password';
-FLUSH PRIVILEGES;
--- srcs/requirements/WordPress/tools/wp-init.sh
+++ srcs/requirements/WordPress/tools/wp-init.sh
@@ -12,7 +12,7 @@ if [ ! -f wp-config.php ]; then
     wp core download --allow-root
 
     mv wp-config-sample.php wp-config.php
-    sed -i "s|database_name_here|${MARIADB_DATABASE}|" wp-config.php
+     sed -i "s|database_name_here|${MARIADB_DATABASE}|" wp-config.php
     sed -i "s|username_here|${MARIADB_USER}|" wp-config.php
     sed -i "s|password_here|${MARIADB_PASSWORD}|" wp-config.php
     sed -i "s|localhost|mariadb|" wp-config.php
@@ -52,93 +52,3 @@ fi
 mkdir -p /run/php
 echo "Starting PHP-FPM..."
 php-fpm7.4 -F
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#set -e
-
-
-#mkdir -p /var/www/html
-#cd /var/www/html
-
-# Télécharger WordPress si le dossier est vide
-#if [ ! -f wp-load.php ]; then
-#  wp core download --allow-root
-#fi
-
-#echo "Variables d'environnement utilisées :"
-#echo "WORDPRESS_DB_NAME=${WORDPRESS_DB_NAME:-$MARIADB_DATABASE}"
-#echo "WORDPRESS_DB_USER=${WORDPRESS_DB_USER:-$MARIADB_USER}"
-#echo "WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD:-$MARIADB_PASSWORD}"
-#echo "WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST:-mariadb}"
-#echo "ta mere0"
-
-# export WORDPRESS_DB_NAME="${WORDPRESS_DB_NAME:-$MARIADB_DATABASE}"
-# export WORDPRESS_DB_USER="${WORDPRESS_DB_USER:-$MARIADB_USER}"
-# export WORDPRESS_DB_PASSWORD="${WORDPRESS_DB_PASSWORD:-$MARIADB_PASSWORD}"
-# export WORDPRESS_DB_HOST="${WORDPRESS_DB_HOST:-mariadb}"
-
-#echo "ta mere1"
-
-#if [ ! -f wp-config.php ]; then
-#  echo "ta mere2"
-#  wp config create \
-#    --dbname="$WORDPRESS_DB_NAME" \
-#    --dbuser="$WORDPRESS_DB_USER" \
-#    --dbpass="$WORDPRESS_DB_PASSWORD" \
-#    --dbhost="$WORDPRESS_DB_HOST" \
-#    --allow-root
-#  echo "ta mere4"
-#fi
-
-# Créer wp-config.php avant de vérifier la base
-
-#echo "ta mere3"
-
-#php-fpm
